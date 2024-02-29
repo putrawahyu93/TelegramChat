7974d00e79a7754ee3c3257607086fb3037d6b88
putrawahyu93 <153715748+putrawahyu93@users.noreply.github.com>
2024-02-29 15:32:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa tanda @
+TOKEN = os.environ.get("6850064319:AAEk1ue9n_dubPh2DfDDsjZ6X-twQHEt0jQ", None) # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "cuvava") # Isi dengan username kalian tanpa tanda @
 GROUP = os.environ.get("GROUP", "aruna_mutual") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
 CHANNEL = os.environ.get("CHANNEL", "gabutan_escape") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
 DB_URI = os.environ.get("DATABASE_URL", "")
